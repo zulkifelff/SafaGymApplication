965e8fa2f4cb59ad05f3b601ac6012c8ee280004
Muhammad Zulkifel <zulkifel@gmail.com>
2021-09-12 19:34:59 +0500

    Initial Commit Phase 3 :zap:

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -23,18 +23,17 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot(
-      [
-        { path: '', component: HomeComponent, pathMatch: 'full' },
-        {
-          path: 'login',
-          loadChildren: () => import('./../app/login/login.module').then(m => m.LoginModule)
-        },
-        {
-          path: 'startup',
-          loadChildren: () => import('./../app/startup/startup.module').then(m => m.StartupModule)
-        }
-    ]),
+    RouterModule.forRoot([
+    { path: '', component: HomeComponent, pathMatch: 'full' },
+    {
+        path: 'login',
+        loadChildren: () => import('./../app/login/login.module').then(m => m.LoginModule)
+    },
+    {
+        path: 'startup',
+        loadChildren: () => import('./../app/startup/startup.module').then(m => m.StartupModule)
+    }
+], { relativeLinkResolution: 'legacy' }),
     BrowserAnimationsModule
   ],
   providers: [],
--- ClientApp/src/app/shared/right-navigation/right-navigation.component.spec.ts
+++ ClientApp/src/app/shared/right-navigation/right-navigation.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { RightNavigationComponent } from './right-navigation.component';
 
@@ -6,7 +6,7 @@ describe('RightNavigationComponent', () => {
   let component: RightNavigationComponent;
   let fixture: ComponentFixture<RightNavigationComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ RightNavigationComponent ]
     })
